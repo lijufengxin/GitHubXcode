5c4022c46f6712c00354ff504fa706e702dbb765
fengxin <fengxin@macdemacbook-pro-2.local>
2018-04-04 11:01:47 +0800

    增加打印测试提交

--- GitHub/ViewController.m
+++ GitHub/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     NSLog(@"github");
     [self reverse:@"nihao"];
+    NSLog(@"fengxin");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
